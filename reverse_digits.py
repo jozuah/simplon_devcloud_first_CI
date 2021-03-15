6163dd165e5f368e2cd2e509668c9bd8b842eeab
Jozuah <galbas.joshua@gmail.com>
2021-03-15 19:43:18 +0100

    Worflow de test 1

--- reverse_digits.py
+++ reverse_digits.py
@@ -8,7 +8,7 @@ def reverse_digits(my_digits):
 			my_digits = my_digits // 10
 	if my_digits < 0 :
 		my_digits = my_digits * (-1)
-                while (my_digits > 0):
+		while (my_digits > 0):
 			remainder = my_digits % 10
 			reverse = (reverse * 10) + remainder
 			my_digits = my_digits // 10
